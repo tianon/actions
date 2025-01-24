4763fe6ad68a3b51359365b3c77049c476f55ec2
Tianon Gravi <admwiggin@gmail.com>
2025-01-23 16:50:09 -0800

    Update `cleanup.sh` with path input support

--- checkout/cleanup.sh
+++ checkout/cleanup.sh
@@ -5,6 +5,12 @@ set -Eeuo pipefail -x
 #env | sort
 
 : host "${host:=${GITHUB_SERVER_URL%/}}"
+path="$PWD${INPUT_PATH:+/${INPUT_PATH#/}}"
+if [ ! -e "$path" ]; then
+	# if our target path doesn't exist, there's nothing to clean
+	exit 0
+fi
+cd "$path"
 
 git config --local --unset "http.$host/.extraheader" || :
 git remote remove origin || :
